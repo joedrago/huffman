0321fe6444c01202b7c6670250ab71aee173c02f
Joe Drago <jdrago@netflix.com>
2021-11-15 22:49:46 -0800

    Add End-Of-Block support

--- huffman.js
+++ huffman.js
@@ -1,53 +1,46 @@
 const fs = require('fs')
 
 class Huffman {
-  generateTable(table, node, code = '') {
+  // ------------------------------------------------------------------------------------
+  // Constructor / Init
+
+  constructor() {
+    this.clear()
+  }
+
+  clear() {
+    this.eob = undefined
+    this.table = {}
+    this.root = {}
+    this.treeBits = 0
+    this.dataBits = 0
+  }
+
+  // ------------------------------------------------------------------------------------
+  // Tree / Table Generation (Encoding)
+
+  generateTableRecurse(node, code) {
     if(node.l) {
-      this.generateTable(table, node.l, code + "0")
-      this.generateTable(table, node.r, code + "1")
+      this.generateTableRecurse(node.l, code + "0")
+      this.generateTableRecurse(node.r, code + "1")
     } else {
       if(code == '') {
         // The input stream is all the same value repeated, just emit 0s
         code = '0'
       }
-      table[node.value] = code
+
+      node.code = code // for debugging
+      if(node.eob) {
+        this.eob = code
+      } else {
+        this.table[node.value] = code
+      }
     }
   }
 
-  writeTree(output, node) {
-    let bitCount = 1
-    if(node.l) {
-      output.push(0)
-      bitCount += this.writeTree(output, node.l)
-      bitCount += this.writeTree(output, node.r)
-    } else {
-      output.push(1)
-      output.push(node.value)
-      bitCount += 8
-    }
-    return bitCount
-  }
-
-  readTree(input) {
-    let which = input.shift()
-    if(which == 0) {
-      // Parent node
-      let l = this.readTree(input)
-      let r = this.readTree(input)
-      return {
-        l: l,
-        r: r,
-      }
-    } else {
-      // Child node
-      let value = input.shift()
-      return {
-        value: value,
-
-        // for debugging
-        char: String.fromCharCode(value),
-      }
-    }
+  generateTable() {
+    this.table = {}
+    this.generateTableRecurse(this.root, '')
   }
 
   generateTree(data) {
@@ -66,8 +59,15 @@ class Huffman {
       occurrenceMap[byte].count += 1
     }
 
-    // Assemble a Huffman tree where the left(0) path is always higher probability than right(1) path
+    // Pull raw entries, add in End-Of-Block
     let occurrences = Object.values(occurrenceMap)
+    occurrences.push({
+      value: 0, // unused
+      count: 0, // Set to 0 to ensure it always has the lowest probability / largest code
+      eob: true,
+    })
+
+    // Assemble a Huffman tree where the left(0) path is always higher probability than right(1) path
     while(occurrences.length > 1) {
       occurrences.sort( (a, b) => a.count - b.count )
 
@@ -82,42 +82,109 @@ class Huffman {
     }
 
     // The only remaining node is the root of the tree
-    let root = occurrences.pop()
-    return root
+    this.root = occurrences.pop()
   }
 
-  encode(data) {
-    let tree = this.generateTree(data)
-    // console.log(JSON.stringify(tree, null, 2))
+  // ------------------------------------------------------------------------------------
+  // Tree Serialization
 
-    let table = {}
-    this.generateTable(table, tree)
-    // console.log(JSON.stringify(table, null, 2))
+  writeTreeRecurse(output, node) {
+    let bitCount = 0
+    if(node.l) {
+      output.push(0)
+      bitCount += 1
+      bitCount += this.writeTreeRecurse(output, node.l)
+      bitCount += this.writeTreeRecurse(output, node.r)
+    } else {
+      output.push(1)
+      bitCount += 1
+      output.push(node.eob ? 1 : 0)
+      bitCount += 1
+      output.push(node.value)
+      bitCount += 8
+    }
+    return bitCount
+  }
+
+  writeTree() {
+    let output = []
+    this.treeBits = this.writeTreeRecurse(output, this.root)
+    return output
+  }
+
+  readTreeRecurse(input) {
+    let which = input.shift()
+    if(which == 0) {
+      // Parent node
+      let l = this.readTreeRecurse(input)
+      let r = this.readTreeRecurse(input)
+      return {
+        l: l,
+        r: r,
+      }
+    } else {
+      // Child node
+      let eob = (input.shift() == 1)
+      let value = input.shift()
+      let node = {
+        value: value,
+
+        // for debugging
+        // char: String.fromCharCode(value),
+      }
+      if(eob) {
+        node.eob = true
+      }
+      return node
+    }
+  }
+
+  readTree(input) {
+    this.root = this.readTreeRecurse(input)
+  }
+
+  // ------------------------------------------------------------------------------------
+  // Encode
+
+  encode(data) {
+    this.clear()
+
+    this.generateTree(data)
+    this.generateTable()
+
+    // console.log(JSON.stringify(this.root, null, 2))
+    // console.log(JSON.stringify(this.table, null, 2))
 
     let dataPayload = ""
     for(let byte of data) {
-      dataPayload += table[byte]
+      dataPayload += this.table[byte]
     }
-    let dataBits = dataPayload.length
+    dataPayload += this.eob
+    this.dataBits = dataPayload.length
 
-    let treePayload = []
-    let treeBits = this.writeTree(treePayload, tree)
+    let treePayload = this.writeTree()
 
     return {
       tree: treePayload,
-      treeBits: treeBits,
       data: dataPayload,
-      dataBits: dataBits,
+
+      treeBits: this.treeBits,
+      dataBits: this.dataBits,
     }
   }
 
+  // ------------------------------------------------------------------------------------
+  // Decode
+
   decode(treePayload, dataPayload) {
-    let tree = this.readTree(treePayload)
-    // console.log(JSON.stringify(tree, null, 2))
+    this.clear()
+
+    this.readTree(treePayload)
+    // console.log(JSON.stringify(this.root, null, 2))
 
     let output = []
     let bits = dataPayload.split('')
-    let n = tree
+    let n = this.root
     for(let bit of bits) {
       if(n.l) {
         if(bit == '0') {
@@ -127,17 +194,25 @@ class Huffman {
         }
       }
 
+      if(n.eob) {
+        // End-of-Block! Bail out
+        console.log("Found End-Of-Block, finishing decode...")
+        break
+      }
+
       if(!n.l) {
         output.push(n.value)
-        n = tree
+        n = this.root
       }
     }
 
     return Buffer.from(output)
   }
+
+  // ------------------------------------------------------------------------------------
 }
 
-main = () => {
+function main() {
   // Read 'data' from an arbitrary binary file (filename in the first arg)
   let argv = process.argv.slice(2)
   if(argv.length < 1) {
